0e322274a22dbfdc07a9a0a62fca3ae229ac8665
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-02-06 22:46:25 -0500

    added character count function

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_counts = get_num_char(text)
     print(f"{num_words} words found in the document")
+    for char, count in char_counts.items():
+        print(f"'{char}': {count}")
 
 
 def get_num_words(text):
@@ -12,6 +15,16 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_num_char(text):
+    lowered_text = text.lower()
+    character_counts = dict()
+    for char in lowered_text:
+        if char in character_counts:
+            character_counts[char] += 1
+        else:
+            character_counts[char] = 1
+    return character_counts
 
 if __name__ == "__main__":
     main()
