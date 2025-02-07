7c9c73e0dc0454c93633447d7e3a8b542b47357b
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-02-06 23:37:29 -0500

    Added function to count and sort characters by usage, formatted into a human-readable report

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import string
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_counts = get_num_char(text)
-    print(f"{num_words} words found in the document")
-    for char, count in char_counts.items():
-        print(f"'{char}': {count}")
+    sorted_char_counts = sort_char_counts(char_counts)
+    
+    # Print the formatted report
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
 
+    for char, count in sorted_char_counts:
+        print(f"The '{char}' character was found {count} times")
+
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -20,11 +28,15 @@ def get_num_char(text):
     lowered_text = text.lower()
     character_counts = dict()
     for char in lowered_text:
-        if char in character_counts:
-            character_counts[char] += 1
-        else:
-            character_counts[char] = 1
+        if char.isalpha():
+            if char in character_counts:
+                character_counts[char] += 1
+            else:
+                character_counts[char] = 1
     return character_counts
+
+def sort_char_counts(character_counts):
+    return sorted(character_counts.items(), key=lambda x: x[1], reverse=True)
 
 if __name__ == "__main__":
     main()
