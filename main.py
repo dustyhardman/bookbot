ac61d011c055e7ecd8f386b633cd454ebd8f6e81
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-02-06 22:13:36 -0500

    Edited for readability

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-def word_count(string):
-    words = string.split()
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(text):
+    words = text.split()
     return len(words)
 
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-
-    a = word_count(file_contents)
-    print(a) 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 if __name__ == "__main__":
     main()
